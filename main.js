9595b36c46672b9b844c8674277f875dbffa4fc1
JonathanNoH <jonathan897h@gmail.com>
2022-03-10 11:45:38 -0700

    add place function

--- main.js
+++ main.js
@@ -1,19 +1,52 @@
 const Player = (name) => {
-    return {};
+
+    
+    return {'name' : name};
 }
 
+
+const playerX = Player('x');
+const playerO = Player('o');
+
 const gameBoard = (() => {
-    let array = ['X', 'O', '', '', 'O', '', 'X', '', 'O'];
+    let array = ['', '', '', '', '', '', '', '', ''];
 
     // cache DOM
     const container = document.querySelector('.container');
-    const squares = container.children;
+    const squares = container.querySelectorAll('div');
+    squares.forEach((elem) => {
+        elem.addEventListener('click', () => {
+            console.log(elem.dataset.box);
+            // ***
+        })
+    })
+
+
+    function place(player, spot) {
+        if (player.name == "x") {
+            array[spot] = 'X';
+        }
+        if (player.name == 'o') {
+            array[spot] = 'O';
+        }
+        render();
+    }
 
     function render() {
         for (let i = 0; i < squares.length; i++) {
             squares[i].innerText = array[i];
         }
     }
-    return {render};
+
+    function checkState() {
+
+    }
+    return {place};
 })()
 
+const gameFlow = (() => {
+    
+    
+
+    return {};
+})()
