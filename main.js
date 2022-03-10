6992a971e3dff2eb32d7f0f3538c94e62d814dad
JonathanNoH <jonathan897h@gmail.com>
2022-03-10 16:08:33 -0700

    can tell when game has been won

--- main.js
+++ main.js
@@ -20,17 +20,17 @@ const gameBoard = (() => {
 
     //private
     const _fillBox = (elem) => {
-        // guard against game not started
-        if (gameState.getCurrentPlayer() == '') return;
+        // guard against game not going
+        if (!gameState.getGameStatus()) return;
 
         // guard against box already filled
         if (array[elem.dataset.box] != '') return;
 
         // get the current player and then place based on which player
-        place(gameState.getCurrentPlayer(), elem.dataset.box);
+        _place(gameState.getCurrentPlayer(), elem.dataset.box);
         gameState.nextPlayer();
         _render();
-        
+        gameState.checkGameOver();
     }
 
 
@@ -40,11 +40,13 @@ const gameBoard = (() => {
         }
     }
 
-    //public
-    const place = (playerName, spot) => {
+    
+    const _place = (playerName, spot) => {
         //places an X or O. Assumes spot empty.
         array[spot] = playerName;
     }
+
+    //public
 
     const getArray = () => array;
 
@@ -53,18 +55,25 @@ const gameBoard = (() => {
         _render();
     }
 
-    return {place, getArray, reset};
+    return {getArray, reset};
 })()
 
 const gameState = (() => {
 
     const playerX = Player('X');
     const playerO = Player('O');
-    let currentPlayer = Player('');
+
+    let isGameGoing = false;
+    const getGameStatus = () => {
+        return isGameGoing;
+    }
+
+    let currentPlayer;
 
     
     //set up start button
     const startGame = () => {
+        isGameGoing = true;
         currentPlayer = playerX;
         updatePlayerDisplay();
         gameBoard.reset();
@@ -82,6 +91,7 @@ const gameState = (() => {
     }
 
     const nextPlayer = () => {
+        //changes player to other player
         if (currentPlayer == playerX) {
             currentPlayer = playerO;
         } else if (currentPlayer == playerO) {
@@ -91,12 +101,46 @@ const gameState = (() => {
         }
         updatePlayerDisplay();
     }
+    const _getWinner = () => {
+        //checks if game is over and returns 'O' or 'X' if there is a winner
+        let array = gameBoard.getArray();
 
+        //create possible lines for winning
+        let row1 = array.slice(0,3);
+        let row2 = array.slice(3,6);
+        let row3 = array.slice(6,9);
+        let col1 = [array[0], array[3], array[6]];
+        let col2 = [array[1], array[4], array[7]];
+        let col3 = [array[2], array[5], array[8]];
+        let dia1 = [array[0], array[4], array[8]];
+        let dia2 = [array[2], array[4], array[6]];
+        let lines = [row1, row2, row3, col1, col2, col3, dia1, dia2];
+
+        function isWinningLine(line) {
+            //line can't be winning if first spot is blank
+            if (line[0] == '') return false;
+            //line is winning if first spot not blank and line matches
+            return ((line[0] == line[1]) && (line[0] == line[2]));
+        }
+
+        for (let line of lines) {
+            //go through each line checking if it is winning
+            if (isWinningLine(line)) {
+                return line[0];
+            }
+        } 
+        return false;
+    }
 
     const checkGameOver = () => {
-
+        let winner = _getWinner();
+        console.log(winner);
+        if (!winner) return;
+        console.log('game over');
+        currentPlayer = '';
+        isGameGoing = false;
     }
 
     
-    return {startGame, getCurrentPlayer, nextPlayer, checkGameOver, updatePlayerDisplay};
+    return {getGameStatus, startGame, getCurrentPlayer, nextPlayer, checkGameOver, updatePlayerDisplay};
 })()
